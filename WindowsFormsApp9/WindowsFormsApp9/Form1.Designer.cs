02e434173d41ce94ad081a0f899fba50f0d2d3e4
eman2312002 <121727617+eman2312002@users.noreply.github.com>
2024-01-09 09:57:52 +0300

    first commet

---
+++ WindowsFormsApp9/WindowsFormsApp9/Form1.Designer.cs
@@ -0,0 +1,145 @@
+﻿namespace WindowsFormsApp9
+{
+    partial class form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.guessTextBox = new System.Windows.Forms.TextBox();
+            this.guessButton = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.messageLabe = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Arial", 19.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.Black;
+            this.label1.Location = new System.Drawing.Point(102, 29);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(608, 40);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "I have a number between 1 and 1000 \r\n";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Arial", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.Black;
+            this.label2.Location = new System.Drawing.Point(267, 151);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(307, 35);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Enter your number : ";
+            // 
+            // guessTextBox
+            // 
+            this.guessTextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F);
+            this.guessTextBox.Location = new System.Drawing.Point(290, 211);
+            this.guessTextBox.Name = "guessTextBox";
+            this.guessTextBox.Size = new System.Drawing.Size(141, 23);
+            this.guessTextBox.TabIndex = 2;
+            // 
+            // guessButton
+            // 
+            this.guessButton.Font = new System.Drawing.Font("Arial", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.guessButton.Location = new System.Drawing.Point(488, 206);
+            this.guessButton.Name = "guessButton";
+            this.guessButton.Size = new System.Drawing.Size(63, 34);
+            this.guessButton.TabIndex = 3;
+            this.guessButton.Text = "Enter ";
+            this.guessButton.UseVisualStyleBackColor = true;
+            this.guessButton.Click += new System.EventHandler(this.guessButton_Click);
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Arial", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Location = new System.Drawing.Point(333, 318);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(195, 64);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "New Game ";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // messageLabe
+            // 
+            this.messageLabe.AutoSize = true;
+            this.messageLabe.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.messageLabe.ForeColor = System.Drawing.Color.Black;
+            this.messageLabe.Location = new System.Drawing.Point(398, 253);
+            this.messageLabe.Name = "messageLabe";
+            this.messageLabe.Size = new System.Drawing.Size(0, 24);
+            this.messageLabe.TabIndex = 6;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Arial", 19.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(180, 78);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(476, 40);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "can you guess my number ?";
+            // 
+            // form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.messageLabe);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.guessButton);
+            this.Controls.Add(this.guessTextBox);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(6)))));
+            this.Name = "form1";
+            this.Text = "Guessing Game";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox guessTextBox;
+        private System.Windows.Forms.Button guessButton;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label messageLabe;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ WindowsFormsApp9/WindowsFormsApp9/Form1.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp9
+{
+    public partial class form1 : Form
+
+    {
+        private int targetNumber;
+        private Random random;
+        private Color defaultBackgroundColor;
+
+        public form1()
+        {
+            InitializeComponent();
+            random = new Random();
+            defaultBackgroundColor = this.BackColor;
+            ResetGame();
+
+        }
+        private void ResetGame()
+        {
+            targetNumber = random.Next(1, 1001);
+            guessTextBox.Text = "";
+            guessTextBox.Enabled = true;
+            guessButton.Enabled = true;
+           
+            this.BackColor = defaultBackgroundColor;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            ResetGame();
+        }
+
+        private void guessButton_Click(object sender, EventArgs e)
+        {
+            int guess;
+            if (int.TryParse(guessTextBox.Text, out guess))
+            {
+                if (guess == targetNumber)
+                {
+                    MessageBox.Show("Correct!");
+                    this.BackColor = Color.Green;
+                    guessTextBox.Enabled = false;
+                    guessButton.Enabled = false;
+                    messageLabe.Text = "Correct!";
+                }
+                else if (guess < targetNumber)
+                {
+                    messageLabe.Text = "Too Low";
+                    this.BackColor = Color.Blue;
+                }
+                else
+                {
+                    messageLabe.Text = "Too High";
+                    this.BackColor = Color.Red;
+                }
+            }
+            else
+            {
+                MessageBox.Show("Invalid input. Please enter a valid number.");
+            }
+            guessTextBox.Text = "";
+        }
+    }
+    }
+
